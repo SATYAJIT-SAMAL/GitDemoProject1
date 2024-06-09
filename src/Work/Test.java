8599951563e54c8cd91eb53661f52939cd9520c7
SATYAJIT-SAMAL <satyajitsamal2400@gmail.com>
2024-06-09 20:43:01 +0530

    First Commit..

---
+++ src/Work/Test.java
@@ -0,0 +1,18 @@
+package Work;
+
+public class Test {
+	public static void main(String args[])
+	{
+	String hungry = "yes";
+	
+	if(hungry=="yes")
+	{
+		System.out.println("Eat Something");
+	}
+	else {
+		System.out.println("Do work");
+		
+	}
+
+}
+}
